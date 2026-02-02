867eedfe773dec806a045e2e1c54c0a9bc13d456
Vineeta Jain <vineetajain@Vineetas-MacBook-Air.local>
2026-02-02 21:47:09 +0530

    backend URL updated

--- backend/app/main.py
+++ backend/app/main.py
@@ -8,6 +8,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",  # frontend
+        "https://tweet-copilot.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
